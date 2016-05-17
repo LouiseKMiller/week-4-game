d90d83c58150ae8fe43fb4d5d44b76e9741cfebb
Louise Miller <louise.austex@gmail.com>
2016-05-17 14:58:14 -0500

    html and css almost complete, js just started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,13 @@
 // javascript file for Assignment 4
 // Buffy RPG game - Louise K Miller
+$(document).ready (function() {
+
+	var audio = new Audio('assets/images/buffyTheme.mp3');
+	audio.play();
+
+	$(".charBlock").click(function() {
+		$("#yourCharDiv").append($("#buffyChar"));
+
+	});
+				
+});
